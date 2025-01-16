bd2d2552d2dd3ccba746b8a31eae8877dda710ce
Marçal Henrique Moreira <mhenrm@gmail.com>
2025-01-15 22:14:37 -0300

    feat: ordened blocks by quicksort algorithm

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ SRC_DIR = src
 CPP_DIR = $(SRC_DIR)/cpp
 H_DIR = $(SRC_DIR)/h
 
-SRCS = $(SRC_DIR)/main.cpp $(CPP_DIR)/InputGenerator.cpp $(CPP_DIR)/BlockCommander.cpp
+SRCS = $(SRC_DIR)/main.cpp $(CPP_DIR)/InputGenerator.cpp $(CPP_DIR)/BlockCommander.cpp $(CPP_DIR)/QuickSort.cpp $(CPP_DIR)/SaveFiles.cpp
 OBJS = $(SRCS:.cpp=.o)
 
 all: $(EXEC)
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,22 @@
 #include "h/InputGenerator.h"
 #include "h/BlockCommander.h"
 
+
 //TODO: Implementar a funcao de ordenação.
 
-using namespace std;
 int main(){
     const std::string filename = "input_data.txt";
+    
 
     // gerar arquivo de entrada
     size_t blockSize = 997;
     std::vector<std::string> blockFiles;
     generateInputFile(filename, 3000, 0, 3000);
 
-    // criar pasta para os blocos
     
-    std::vector<std::vector<int>> blocks;
+    std::vector<std::vector<int>> sortedBlocks;
     BlockCommander blockCommander;
-    blocks = blockCommander.splitBlock(filename, blockSize, blockFiles);
-
-    // for (size_t i = 0; i < blocks.size(); i++) {
-    //     std::cout << "Block " << i << ": " << std::endl;
-    //     for (int num: blocks[i]) {
-    //         std::cout << num << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
+    sortedBlocks = blockCommander.sortBlocks(filename, blockSize);
 
     return 0;
 }
